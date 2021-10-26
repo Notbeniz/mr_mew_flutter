b5be4cb5979023801647ff1dfde2ee59ccd58b0f
Zeynab S Alavi <dvlpr1992@gmail.com>
2021-10-27 00:03:39 +0330

    part 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,60 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
-
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color(0xff01579B),
         appBar: AppBar(
-          title: const Text('Lucifer  👹'),
-          backgroundColor: Colors.pinkAccent,
+          title: const Text(
+            'dvlpr1992👩🏽‍💻',
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'RobotoBlack',
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5),
+          ),
+          backgroundColor: Colors.amber[400],
         ),
         body: SafeArea(
+            child: IntrinsicWidth(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('images/Mr._Mew.png'),
+            // mainAxisAlignment: MainAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.all(20.0),
+                child: Image.asset('images/beniz.JPG'),
+              ),
+              const Text(
+                'Beniz',
+                style: TextStyle(
+                    fontFamily: 'Chalkduster',
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                    letterSpacing: 2.0),
+              ),
             ],
           ),
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.amber[400],
+          child: const Icon(Icons.add),
+          foregroundColor: Colors.black,
         ),
       ),
       debugShowCheckedModeBanner: false,
-    ),
-  );
+    );
+  }
 }
